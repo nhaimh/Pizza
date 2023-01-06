e3750d1d63e9eb9d0d3261a60d97a2e012463e5e
phong310 <phamdinhphong12@gmail.com>
2023-01-06 14:26:23 +0700

    update

--- client/src/components/DetailsProduct/DetailsProduct.js
+++ client/src/components/DetailsProduct/DetailsProduct.js
@@ -1,101 +1,114 @@
-import './DetailsProduct.css';
-import { useState } from 'react';
-import { useParams, Link } from 'react-router-dom';
+import "./DetailsProduct.css";
+import { useState } from "react";
+import { useParams, Link } from "react-router-dom";
 
-import { connect } from 'react-redux';
-import { addCart } from '../../redux/actions/cartActions';
-import { deleteProduct } from '../../redux/actions/productsActions';
-import { useNavigate } from 'react-router-dom';
+import { connect } from "react-redux";
+import { addCart } from "../../redux/actions/cartActions";
+import { deleteProduct } from "../../redux/actions/productsActions";
+import { useNavigate } from "react-router-dom";
 
 const DetailsProduct = (props) => {
+  const [modal, setModal] = useState(false);
 
-    const [modal, setModal] = useState(false);
+  let [image, setImage] = useState(true);
+  let [text, setText] = useState("");
 
-    let [image, setImage] = useState(true);
-    let [text, setText] = useState('');
+  let params = useParams();
+  let history = useNavigate();
 
-    let params = useParams();
-    let history = useNavigate();
+  const product = props.products.find((prod) => prod._id === params.id);
 
-    const product = props.products.find(prod => prod._id === params.id);
+  const deleteHandler = () => {
+    props.deleteProduct(product._id, history);
+  };
 
-    const deleteHandler = () => {
-        props.deleteProduct(product._id, history)
-    }
+  function addToCart(product) {
+    setText("Added");
+    setImage(false);
 
+    setTimeout(() => {
+      setText("");
+      setImage(true);
+    }, 500);
 
-    function addToCart(product) {
+    props.addCart(product);
+  }
 
-        setText('Added');
-        setImage(false);
+  if (product) {
+    return (
+      <>
+        {modal ? (
+          <div className="modal">
+            <div className="modalContent">
+              <p id="txtModal">Do you want to delete this product?</p>
+              <button id="modalDel" onClick={deleteHandler}>
+                Delete
+              </button>
+              <button id="modalCancel" onClick={() => setModal(false)}>
+                Cancel
+              </button>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
 
-        setTimeout(() => {
-            setText('')
-            setImage(true)
-        }, 500)
+        <div id="detailsId">
+          <h2>{product.title}</h2>
+          <img src={product.imageUrl} id="detailsImage" alt="productDetails" />
+          <p>
+            <b>{product.description}</b>
+          </p>
+          <p id="price">Price: {product.price} $</p>
+          <div id="detailBtns">
+            {props.user && (
+              <>
+                {image && (
+                  <div
+                    className="imgCartDetails"
+                    onClick={() => addToCart(product)}
+                  />
+                )}
+                {text ? <p id="txtDetails">{text}</p> : ""}
 
-        props.addCart(product)
+                {(props.user.role === "editor" ||
+                  props.user.role === "admin") && (
+                  <>
+                    <Link
+                      to={`/details/edit/${product._id}`}
+                      id="detailsEditBtn"
+                    >
+                      Edit
+                    </Link>
+                    <button
+                      id="detailsDeleteBtn"
+                      onClick={() => setModal(true)}
+                    >
+                      Delete
+                    </button>
+                  </>
+                )}
+              </>
+            )}
+            <Link to="/" id="detailsBackBtn">
+              Back
+            </Link>
+          </div>
+        </div>
+      </>
+    );
+  }
 
-
-    }
-
-    if (product) {
-        return (
-            <>
-                {modal ? (
-
-                    <div className='modal'>
-                        <div className='modalContent'>
-                            <p id='txtModal'>Do you want to delete this product?</p>
-                            <button id="modalDel" onClick={deleteHandler}>Delete</button>
-                            <button id='modalCancel' onClick={() => setModal(false)}>Cancel</button>
-                        </div>
-
-                    </div>)
-                    : ''
-                }
-
-                <div id='detailsId'>
-                    <h2>{product.title}</h2>
-                    <img src={product.imageUrl} id='detailsImage' alt='productDetails' />
-                    <p><b>{product.description}</b></p>
-                    <p id='price'>Price: {product.price} lv.</p>
-                    <div id='detailBtns'>
-
-                        {props.user &&
-                            <>
-                                {image && <div className='imgCartDetails' onClick={() => addToCart(product)} />}
-                                {text ? <p id='txtDetails'>{text}</p> : ''}
-
-                                {(props.user.role === "editor" || props.user.role === 'admin') &&
-                                    <>
-                                        <Link to={`/details/edit/${product._id}`} id="detailsEditBtn">Edit</Link>
-                                        <button id="detailsDeleteBtn" onClick={() => setModal(true)}>Delete</button>
-                                    </>
-                                }
-
-                            </>
-                        }
-                        <Link to='/' id="detailsBackBtn">Back</Link>
-
-                    </div>
-
-                </div>
-            </>
-
-        )
-    }
-
-    return null
-
-}
+  return null;
+};
 
 const mapStateToProps = (state) => {
-    return {
-        user: state.auth.user,
-        products: state.products.data
-    }
+  return {
+    user: state.auth.user,
+    products: state.products.data,
+  };
+};
 
-}
-
-export default connect(mapStateToProps, { addCart, deleteProduct })(DetailsProduct);
+export default connect(mapStateToProps, { addCart, deleteProduct })(
+  DetailsProduct
+);
